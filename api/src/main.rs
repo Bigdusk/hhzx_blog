ec4e0f3b41b15182fd0ea6e7d3f463202107cb7f
hhzx <2831828656@qq.com>
2024-04-21 20:14:08 +0800

    系统初始化

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use axum_server::tls_rustls::RustlsConfig;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, world!" }));
+
+    let config = RustlsConfig::from_pem_file(
+        "self-signed-certs/cert.pem",
+        "self-signed-certs/key.pem",
+    )
+        .await
+        .unwrap();
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 2002));
+    println!("Run server https://{}", addr);
+    axum_server::bind_rustls(addr, config)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
